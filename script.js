876b083e513d9337fde00c94527d647a35914270
Jako <43934830+jfrb95@users.noreply.github.com>
2025-02-23 18:33:50 +0000

    fix header background, add shadows

--- script.js
+++ script.js
@@ -1,3 +1,7 @@
-//NEXT: find good way to scale size of project boxes 
-// - there is a way to somehow get them to change size a bit while also
-// - wrapping as and when
+//TODO: 
+// - find good way to scale size of project boxes 
+//   - there is a way to somehow get them to change size a bit while also
+//   - wrapping as and when.
+// - make things (e.g. header image) scale slower than the 
+//   - size of the screen in between breakpoints
+// - header background is not perfect for all screen sizes
